3b2eb66a8153c1f9f52e90208b327af870f818b2
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-04-03 11:33:33 +0530

    hero

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -59,9 +59,7 @@ const Hero = () => {
             </button>
             <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                <Link
-                  href={"file:///F:/Resume-Shravani-Chendwankar.pdf"}
-                > Download CV</Link>
+              Download CV         
               </span>
             </button>
           </div>
