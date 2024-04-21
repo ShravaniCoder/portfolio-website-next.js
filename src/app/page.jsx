638bca8f6d408709a1e5a198b60819935b3a11c6
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-04-21 16:22:27 +0530

    yes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -65,7 +65,7 @@ const Homepage = () => {
             <button className="px-1 py-1 rounded-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-800 text-white mt-3">
               <Link
                 href="https://drive.google.com/file/d/1LUIqkxB1hIHL9NHlFeTYBnqKWoCdPcfp/view?usp=sharing"
-                download="MyResume.pdf"
+                download 
               >
                 <span className="flex items-center gap-x-2 bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                   My Resume
